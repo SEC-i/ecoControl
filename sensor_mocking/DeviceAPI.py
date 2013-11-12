fc96abfa9fac86ea9ed4f7976cd541d28a564bba
Max Reimann <max.reimann@student.hpi.uni-potsdam.de>
2013-11-12 12:06:34 +0100

    setting workload now possible in DeviceAPI

--- sensor_mocking/DeviceAPI.py
+++ sensor_mocking/DeviceAPI.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import BHKW
 
 app = Flask(__name__)
@@ -21,17 +21,30 @@ def get_sensor(device_id,sensor_id):
     } 
     return jsonify( { 'sensor': sensor_data } )
 
-@app.route('/device/<int:device_id>', methods = ['GET'])
-def get_device(device_id):
-    device = next(dev for dev in devices if dev.device_id == device_id)
-    device_data = { "device_id" : device_id }
+@app.route('/device/<int:device_id>', methods = ['GET','POST'])
+def get_or_set_device(device_id):
+    if request.method == 'POST':
+        if 'workload' in request.form:
+            return set_workload(device_id,request.form['workload'])
+    else:
+        device = next(dev for dev in devices if dev.device_id == device_id)
+        device_data = { "device_id" : device_id }
+        for sensor in device.sensors:
+            #sensor  = device.getMappedSensor(sensor.id)
+            device_data[sensor.name] = sensor.value
+        return jsonify( {"device" : device_data})
 
-    for sensor_id in range(len(device.sensors)):
-        sensor  = device.getMappedSensor(sensor_id)
-        device_data[sensor.name] = sensor.value
-    return jsonify( {"device" : device_data})
 
 
+def set_workload(device_id,workload):
+    device = [dev for dev in devices if dev.device_id == device_id ][0]
+    workload = float(workload)
+    
+    if workload >= 0 and workload <= 100:
+        device.setcurrentWorkload(workload)
+        return "1"
+    else:
+        return "0"
 
 
 
