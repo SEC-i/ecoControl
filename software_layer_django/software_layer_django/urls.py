ec1dfbbcc3a2485452c1af841d11684e26384851
fabian <fabian@bp2013h1ws02.hpi.uni-potsdam.de>
2013-11-13 17:47:50 +0100

    create software layer with django

--- software_layer_django/software_layer_django/urls.py
+++ software_layer_django/software_layer_django/urls.py
@@ -1,17 +1,23 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+from webapi import views
 
 urlpatterns = patterns('',
+    (r'^api/$', views.index), #'software_layer_django.views.index', name='index'),
+    (r'^api/devices/(limit/(?P<limit>\d+)/)?$', views.list_devices),
+    (r'^api/device/(?P<device_id>\d+)/$', views.show_device),
+    (r'^api/device/(?P<device_id>\d+)/sensors/(limit/(?P<limit>\d+)/)?$', views.list_sensors),
+    (r'^api/device/(?P<device_id>\d+)/entries/(limit/(?P<limit>\d+)/)?$', views.list_entries),
+    (r'^api/sensor/(?P<sensor_id>\d+)/$', views.show_sensor),
+    (r'^api/sensor/(?P<sensor_id>\d+)/entries/(limit/(?P<limit>\d+)/)?$', views.list_sensor_entries),
+    (r'^api/entry/(?P<entry_id>\d+)/$', views.show_entry),
+    (r'^api/device/(?P<device_id>\d+)/set/$', views.set_device),
     # Examples:
     # url(r'^$', 'software_layer_django.views.home', name='home'),
-    # url(r'^software_layer_django/', include('software_layer_django.foo.urls')),
+    # url(r'^blog/', include('blog.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
---
+++ software_layer_django/software_layer_django/webapi/models.py
@@ -0,0 +1,27 @@
+import json, calendar
+
+from django.db import models
+
+class Device(models.Model):
+    name = models.CharField(max_length = 100)
+    
+    def to_dict(self):
+        return { 'id':self.id, 'name':self.name}
+    
+class Sensor(models.Model):
+    device = models.ForeignKey('Device')
+    name = models.CharField(max_length = 100)
+    unit = models.CharField(max_length = 50)
+    
+    def to_dict(self):
+        return { 'id':self.id, 'device_id':self.device.id, 'name':self.name, 'unit':self.unit}
+    
+class SensorEntry(models.Model):
+    sensor = models.ForeignKey('Sensor')
+    value = models.CharField(max_length = 200)
+    timestamp = models.DateTimeField(auto_now = True)
+    
+    def to_dict(self):
+        return { 'id':self.id, 'sensor_id':self.sensor.id, 'value':self.value, 'timestamp':calendar.timegm(self.timestamp.utctimetuple())}
+    
+
