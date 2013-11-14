70859c23b5993668ea4e9cacc8cfc97669600b87
Fabio Niephaus <fabio@idea4IT.local>
2013-11-14 11:03:18 +0100

    Register models in admin backend

--- software_layer_django/software_layer_django/webapi/admin.py
+++ software_layer_django/software_layer_django/webapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from software_layer_django.webapi.models import Device, Sensor, SensorEntry
 
-# Register your models here.
+# Register models
+admin.site.register(Device)
+admin.site.register(Sensor)
+admin.site.register(SensorEntry)
+
+
