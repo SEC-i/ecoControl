fd9efeeb73e440b7cf0cf14ce12b8ddd2ecaa5c7
Fabio Niephaus <fabio@idea4IT.local>
2013-11-23 11:52:44 +0100

    Base URL changed to external address
    The Plant Frontend can now be seen from everywhere on the planet ;)

--- Julia/Plant Frontend/button_action.js
+++ Julia/Plant Frontend/button_action.js
@@ -6,7 +6,7 @@ var base_url_get = "http://172.16.19.114/data/"; //pi
 
 //var base_url_get = "http://141.89.75.185/api/"
 
-var base_url_get = "http://172.16.64.130/api/" // VM IP
+var base_url_get = "http://www.hpi.uni-potsdam.de/hirschfeld/bachelorprojects/2013H1/api/" // VM IP
 
 
 $(document).ready(function(){
