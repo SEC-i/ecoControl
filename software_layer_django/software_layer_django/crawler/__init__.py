04c3ac2e6e670a5ddcaaa348104d2b4113de16d5
Fabio Niephaus <fniephaus@users.noreply.github.com>
2013-11-26 18:59:32 +0100

    Indents PEP 8 conform

--- software_layer_django/software_layer_django/crawler/__init__.py
+++ software_layer_django/software_layer_django/crawler/__init__.py
@@ -9,29 +9,29 @@ logger = logging.getLogger('crawler')
 
 # Crawler thread class
 class Crawler(threading.Thread):
-	def __init__(self, frequency=1):
-		threading.Thread.__init__(self)
-		self.daemon = True
-		self.frequency = frequency
-		#make sure only one instance is running at a time
-		self.is_unique_thread = True
-		if not write_pidfile_or_fail("/tmp/crawler.pid"):
-			self.is_unique_thread = False
-		# start thread immediately
-		self.start()
-	def run(self):
-		if self.is_unique_thread:
-			# add new measurement approx. every minute
-			print " * Crawling..."
-			logger.debug("Crawler started")
-			while(True):
-				crawl_and_save_data()
+    def __init__(self, frequency=1):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.frequency = frequency
+        #make sure only one instance is running at a time
+        self.is_unique_thread = True
+        if not write_pidfile_or_fail("/tmp/crawler.pid"):
+            self.is_unique_thread = False
+        # start thread immediately
+        self.start()
+    def run(self):
+        if self.is_unique_thread:
+            # add new measurement approx. every minute
+            print " * Crawling..."
+            logger.debug("Crawler started")
+            while(True):
+                crawl_and_save_data()
 
-				# log function calls if frequency >= 10 seconds
-				if self.frequency>9:
-					logger.debug("Crawl function called")
-				
-				time.sleep(self.frequency)
-		else:
-			logger.debug("Duplicate crawler thread avoided")
-			print " * Duplicate crawler thread avoided"
+                # log function calls if frequency >= 10 seconds
+                if self.frequency>9:
+                    logger.debug("Crawl function called")
+                
+                time.sleep(self.frequency)
+        else:
+            logger.debug("Duplicate crawler thread avoided")
+            print " * Duplicate crawler thread avoided"
--- software_layer_django/software_layer_django/planner/__init__.py
+++ software_layer_django/software_layer_django/planner/__init__.py
@@ -10,30 +10,30 @@ logger = logging.getLogger('planner')
 
 # Crawler thread class
 class Planner(threading.Thread):
-	def __init__(self, frequency=1):
-		threading.Thread.__init__(self)
-		self.daemon = True
-		self.frequency = frequency
-		#make sure only one instance is running at a time
-		self.is_unique_thread = True
-		if not write_pidfile_or_fail("/tmp/planner.pid"):
-			self.is_unique_thread = False
-		# start thread immediately
-		self.start()
-	def run(self):
-		if self.is_unique_thread:
-			# add new measurement approx. every minute
-			print " * Crawling..."
-			logger.debug("planner started")
-			while(True):
-				update_delta()
-				check_rules()
+    def __init__(self, frequency=1):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.frequency = frequency
+        #make sure only one instance is running at a time
+        self.is_unique_thread = True
+        if not write_pidfile_or_fail("/tmp/planner.pid"):
+            self.is_unique_thread = False
+        # start thread immediately
+        self.start()
+    def run(self):
+        if self.is_unique_thread:
+            # add new measurement approx. every minute
+            print " * Crawling..."
+            logger.debug("planner started")
+            while(True):
+                update_delta()
+                check_rules()
 
-				# log function calls if frequency >= 10 seconds
-				if self.frequency>9:
-					logger.debug("Crawl function called")
-				
-				time.sleep(self.frequency)
-		else:
-			logger.debug("Duplicate planner thread avoided")
-			print " * Duplicate planner thread avoided"
+                # log function calls if frequency >= 10 seconds
+                if self.frequency>9:
+                    logger.debug("Crawl function called")
+                
+                time.sleep(self.frequency)
+        else:
+            logger.debug("Duplicate planner thread avoided")
+            print " * Duplicate planner thread avoided"
