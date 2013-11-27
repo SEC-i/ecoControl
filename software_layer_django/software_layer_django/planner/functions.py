4e51dc458a25a74aab534ceec800e1ee1adb1ff1
Max Reimann <max.reimann@student.hpi.uni-potsdam.de>
2013-11-27 10:56:15 +0100

    unittest for planner

--- software_layer_django/software_layer_django/planner/functions.py
+++ software_layer_django/software_layer_django/planner/functions.py
@@ -1,18 +1,29 @@
 import logging
-
+from datetime import datetime
 from software_layer_django.models import Sensor, SensorEntry, SensorRule, SensorDelta, Task
 
 logger = logging.getLogger('planner')
 
 # crawl data and save sensor entries
 def update_delta():
-	logger.debug("update delta")
-	
+    logger.debug("update delta")
+
 
 
 def check_rules():
-	pass
+    sensor_rules = SensorRule.Objects.all()
+    for rule in sensor_rules:
+        sensor_value = SensorEntry.Objects.all().filter(sensor= rule.Sensor).order_by('-timestamp')[0]
+        condition_string = str(sensor_value) + str(rule.comparison) + str(rule.threshold)
+        if eval(condition_string) == True:
+            create_or_update_task()
 
 def create_or_update_task():
-	pass
+    logger.debug("updating or creating task")
 
+
+
+def check_tasks():
+    for task in Task.Objects.all():
+        if task.execution_timestamp <= datetime.now():
+            logger.debug("execute task" + task.command)
