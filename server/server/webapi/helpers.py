911dce9bb8816af0837d4703f81a6e27467fc863
Fabio Niephaus <fniephaus@users.noreply.github.com>
2013-12-03 12:27:11 +0100

    JSONP support added

--- server/server/webapi/helpers.py
+++ server/server/webapi/helpers.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse
 from django.core.serializers.json import DjangoJSONEncoder
 from django.contrib.auth.models import Group
 
-def create_json_response(data):
-    response = HttpResponse(json.dumps(data, cls=DjangoJSONEncoder), content_type='application/json')
+def create_json_response(request, data):
+    if 'callback' in request.GET:
+        response = HttpResponse( request.GET['callback'] + "(" + json.dumps(data, cls=DjangoJSONEncoder) + ");", content_type='application/json')
+    else:
+        response = HttpResponse(json.dumps(data, cls=DjangoJSONEncoder), content_type='application/json')
     return response
 
-def create_json_response_from_QuerySet(data):
-    return create_json_response(list(data.values()))
+def create_json_response_from_QuerySet(request, data):
+    return create_json_response(request, list(data.values()))
 
 def is_in_group(user, group_id):
     """
