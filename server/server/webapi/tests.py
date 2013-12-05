15b964d406782d6240009f5c38c8f693b5736da6
Fabio Niephaus <fniephaus@users.noreply.github.com>
2013-12-05 18:07:01 +0100

    Tests updated

--- server/server/webapi/tests.py
+++ server/server/webapi/tests.py
@@ -20,7 +20,7 @@ class SimpleTest(unittest.TestCase):
 
     def test_login_inactive(self):
         # Issue a GET request.
-        response = self.client.get('/api/status/')
+        response = self.client.get('/status/')
 
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
@@ -30,7 +30,7 @@ class SimpleTest(unittest.TestCase):
 
     def test_login_procedure(self):
         # Issue a GET request.
-        response = self.client.post('/api/login/', {'username': 'test_user', 'password': 'demo123'})
+        response = self.client.post('/login/', {'username': 'test_user', 'password': 'demo123'})
 
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
@@ -39,7 +39,7 @@ class SimpleTest(unittest.TestCase):
         self.assertEqual(json.loads(response.content), {"login": "successful", "user": "test_fn test_ln"})
 
         # Issue a GET request.
-        response = self.client.get('/api/status/')
+        response = self.client.get('/status/')
 
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
@@ -50,7 +50,7 @@ class SimpleTest(unittest.TestCase):
     def test_permission_denied(self):
         # test multipe urls for {"permission": "denied"}
         for url in ['devices/', 'device/1/', 'device/1/sensors/', 'device/1/entries/', 'sensor/1/', 'sensor/1/entries/', 'entry/1/', 'device/1/']:
-            response = self.client.get('/api/' + url)
+            response = self.client.get('/' + url)
             self.assertEqual(json.loads(response.content), {"permission": "denied"})
 
     def test_add_device(self):
@@ -59,7 +59,7 @@ class SimpleTest(unittest.TestCase):
 
         # Issue GET request for device details
         d = Device.objects.get(name="test_name")
-        response = self.client.get('/api/device/' + str(d.id) + '/')
+        response = self.client.get('/device/' + str(d.id) + '/')
 
         # Check that the response contains the test device
         self.assertEqual(json.loads(response.content), [{"data_source": "test_data_source", "id": 1, "name": "test_name", "interval": 60}])
