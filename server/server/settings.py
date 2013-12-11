e3a1e721e9be4de9292bfb4c2576e56ecbc67c50
Fabio Niephaus <fniephaus@users.noreply.github.com>
2013-12-11 14:27:57 +0100

    Static files disabled in Django's settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'server',
     'server.webapi',
 )
@@ -92,15 +91,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-# STATIC_URL = '/static/'
-# STATIC_ROOT = ''
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "../website"),
-# )
 
 # Logging
 
