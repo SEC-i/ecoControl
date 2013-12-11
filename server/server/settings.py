e9e382e1f6467d2f5542d50614de23564706fef1
Fabio Niephaus <fniephaus@users.noreply.github.com>
2013-12-11 17:23:28 +0100

    Push activated, misc bugfixes

--- server/server/settings.py
+++ server/server/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'server',
     'server.webapi',
 )
