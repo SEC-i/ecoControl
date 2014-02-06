f037ca1be14e90da84d6b4e11e22dab0ff1e506d
Max Reimann <max.reimann@student.hpi.uni-potsdam.de>
2014-02-06 13:44:58 +0100

    simulation parameters

--- simulation/heating.py
+++ simulation/heating.py
@@ -38,19 +38,17 @@ class Heating(Device):
         time_delta_hour = time_delta / milliseconds_per_hour
         self.heat_loss(time_delta)
         
-        change_speed = 0.01
+        change_speed = 0.001
         rand = change_speed * (random.random() * 2.0 - 1.0)
         slope = change_speed * sign(self.target_temperature - self.sensors["temperature"].value)
-        power_delta = (rand + slope ) * time_delta_hour
+        power_delta = (rand + slope ) * time_delta
 
         self.current_power += power_delta
         self.current_power =  max(min(self.current_power, self.max_power),0)
         
         
-        
-        if self.sensors["temperature"].value < self.target_temperature:
-            heat_storage.consume_energy(self.current_power / 1000 * time_delta_hour)
-            self.heat_room(time_delta)
+        heat_storage.consume_energy(self.current_power / 1000 * time_delta_hour)
+        self.heat_room(time_delta)
         
 
     def heat_loss(self, time_delta):
