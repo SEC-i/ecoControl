bfcf4cc077c903c96bee5f2a8a10bad1ee9e7587
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-02-10 13:56:10 +0100

    Simulation-related code removed

--- website/static/js/ready.js
+++ website/static/js/ready.js
@@ -1,17 +1,9 @@
 var api_url = "http://www.hpi.uni-potsdam.de/hirschfeld/bachelorprojects/2013H1/api/";
-var simulation_api_url = "http://www.hpi.uni-potsdam.de/hirschfeld/bachelorprojects/2013H1/simulation/";
 
 var device_data = null;
 var device_id = null;
 var range_start = new Date().getTime()-24*2*60*60*1000;
 var range_end = new Date().getTime();
-
-// simulation-related
-var bhkw_info = null;
-var hs_info = null;
-var rad_info = null;
-var electric_consumer_info = null;
-var plb_info = null;
 
 $(document).ready(function(){
     // Login events
@@ -42,32 +34,6 @@ $(document).ready(function(){
     $("#home_button").click(function(event) {
         hide_all();
         $("#home").fadeIn();
-    });
-
-    $("#simulation_button").click(function(event) {
-        hide_all();
-        $("#simulation").fadeIn();
-
-        $.getJSON( simulation_api_url + "device/0/info", function( data ) {
-            bhkw_info = data;
-        });
-        $.getJSON( simulation_api_url + "device/1/info", function( data ) {
-            hs_info = data;
-        });
-        $.getJSON( simulation_api_url + "device/2/info", function( data ) {
-            rad_info = data;
-        });
-        $.getJSON( simulation_api_url + "device/3/info", function( data ) {
-            electric_consumer_info = data;
-        });
-        $.getJSON( simulation_api_url + "device/4/info", function( data ) {
-            plb_info = data;
-        });
-
-        setInterval(function(){
-            refresh_simulation();
-        }, 1000);
-
     });
 
     check_login_status();
@@ -103,7 +69,6 @@ function show_login_box(){
 function hide_all(){
     $("#home").hide();
     $("#devices").hide();
-    $("#simulation").hide();
 }
 
 function login_user() {
@@ -134,7 +99,6 @@ function login_successful(){
        }
     }).done(function( data ) {
         device_data = data;
-        device_data.push({"id":9999,"name":"Simulation"});
         $("#device_list").html(''); // clear device list
         $.each(device_data, function(index, value){
             $("#device_list").append('<li class="device_items" id="device_item_' + value['id'] + '"><a onclick="show_device(' + value['id'] + ', \'' + value['name'] + '\');">' + value['name'] + '</a></li>');
@@ -168,11 +132,7 @@ function show_device(id, device_name) {
     $("#diagram_container").html('');
     $("#sensor_selection").html('');
     
-    if(id == 9999){
-        prepare_simulation_diagram();
-    }else{
-        prepare_diagram(device_id);
-    }
+    prepare_diagram(device_id);
 }
 
 function prepare_diagram(device_id){
