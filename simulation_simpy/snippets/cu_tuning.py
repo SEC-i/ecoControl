c4d52b747bbe2e04e6171ab81bd606ab31539677
steier <jksteier@gmail.com>
2014-02-25 17:32:03 +0100

    code snippets of wiki added

--- simulation_simpy/snippets/cu_tuning.py
+++ simulation_simpy/snippets/cu_tuning.py
@@ -3,3 +3,6 @@ if (thermal_consumer.get_outside_temperature() > 10):
 
 if (heat_storage.get_temperature() >= heat_storage.max_temperature - 5):
     cu.workload = 40
+    
+    
+# BHKW wird hier nur noch 1 mal statt 8000 mal ausgeschaltet, etwa gleiche Kostenbilanz
