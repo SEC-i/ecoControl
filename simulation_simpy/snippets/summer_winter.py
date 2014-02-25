0aeee19c83a48de7e8ad554d968a4e9a1724a32b
steier <jksteier@gmail.com>
2014-02-25 17:34:36 +0100

    code snippets of wiki added

--- simulation_simpy/snippets/summer_winter.py
+++ simulation_simpy/snippets/summer_winter.py
@@ -5,3 +5,6 @@ else:
 
     if (heat_storage.get_temperature() >= heat_storage.max_temperature - 5):
         cu.workload = 40
+
+
+# Sommer-Winter-regel. Funktioniert noch nicht so gut (etwa 200 Euro schlechter als ohne Regeln
