1b9237797a4c86fb16531dba5be85a47568f6264
Eva-Maria Herbst <eva-maria.herbst@student.hpi.uni-potsdam.de>
2014-04-09 14:43:00 +0200

    test: current_power is changed correctly during simulate consumption

--- simulation/testing/consumers_tests.py
+++ simulation/testing/consumers_tests.py
@@ -9,6 +9,11 @@ from environment import ForwardableRealtimeEnvironment
 from systems.storages import HeatStorage
 
 class ThermalConsumerTests(unittest.TestCase):
+    def setUp(self):
+        env = ForwardableRealtimeEnvironment()
+        heat_storage = HeatStorage(env)
+        self.consumer = ThermalConsumer(env, heat_storage)
+ 
     '''def test_step(self):
        # pass
         verbrauch wird berechnet 
@@ -27,8 +32,47 @@ class ThermalConsumerTests(unittest.TestCase):
         # sets self.current_power
         # encreases the current power if its too cold, else decreases
         it'''
+    
+    def test_simulate_consumption_increase_current_power(self):
+        '''the current_power should be increased if the current temperature 
+        is below the target temperature'''
+        self.consumer.temperature_room = 0
+        self.consumer.target_temperature = 30
+        last_current_power = 20
+        self.consumer.current_power = last_current_power
+        
+        self.consumer.simulate_consumption()
+        
+        self.assertGreater(self.consumer.current_power, last_current_power)
+        
+    def test_simulate_consumption_decrease_current_power(self):
+        '''the current_power should be decreased if the current temperature 
+        is below the target temperature'''
+        self.consumer.temperature_room = 30
+        self.consumer.target_temperature = 0
+        last_current_power = 20
+        self.consumer.current_power = last_current_power
+        
+        self.consumer.simulate_consumption()
+        
+        self.assertLess(self.consumer.current_power, last_current_power)
+        
+        
+    def test_room_power_simulate_consumption(self):
+        # sets self.room_power power,
+        # the current power of the heating of the room
+        # the value comes from the former current_power
+    
+        '''def test_temperature_room(self):
+        # during simulate_consumption
+        # set by calculate_room_temperature
+        # depends on room_power,
+        # heat capacity
+        # time delta'''
         
     def test_target_temperature_simulate_consumption(self):
+        '''the target temperature of the consumer should be set
+        according to the daily demand'''
         daily_demand = [19, 19, 3, 19, 6, 19, 19, 20, 21, 20, 20, 21, 
             20, 21, 21, 21, 21, 22, 22, 5, 22, 22, 21, 19]
     
