4092a04b06b5f924b08fbf4f77ba69b02f0030c3
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-04-14 19:28:32 +0200

    Fix typos

--- simulation/testing/functional_tests.py
+++ simulation/testing/functional_tests.py
@@ -6,14 +6,14 @@ from forecasting_script import Forecast
 
 class ForecastTestCase(unittest.TestCase):
 
-    def test_forecast_api_provides_inaccurracy(self):
+    def test_forecast_api_provides_inaccuracy(self):
         rv = start.app.test_client().get('/api/forecasts/')
-        self.assertIn('forecast_inaccurracy', rv.data)
+        self.assertIn('forecast_inaccuracy', rv.data)
 
-    def test_forecast_has_inaccurracy(self):
+    def test_forecast_has_inaccuracy(self):
         fcast = Forecast()
         try:
-            inaccurracy = fcast.get_current_inaccurency()
+            inaccuracy = fcast.get_current_inaccurency()
         except AttributeError:
             self.fail(
                 "the forecasting should the method get_current_inaccurency'")
