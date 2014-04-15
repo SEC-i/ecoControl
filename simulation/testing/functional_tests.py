78ce17f6f0b0c8f222ef5913489475957eeb7d77
Eva-Maria Herbst <eva-maria.herbst@student.hpi.uni-potsdam.de>
2014-04-15 10:04:32 +0200

    debug message to prevent printing of a whole dictionary

--- simulation/testing/functional_tests.py
+++ simulation/testing/functional_tests.py
@@ -8,7 +8,8 @@ class ForecastTestCase(unittest.TestCase):
 
     def test_forecast_api_provides_inaccuracy(self):
         rv = start.app.test_client().get('/api/forecasts/')
-        self.assertIn('forecast_inaccuracy', rv.data)
+        self.assertIn('forecast_inaccuracy', rv.data, 
+            "the forecast data should contain a value for the inaccuracy of the forecast")
 
     def test_forecast_has_inaccuracy(self):
         fcast = Forecast()
@@ -16,4 +17,7 @@ class ForecastTestCase(unittest.TestCase):
             inaccuracy = fcast.get_current_inaccurency()
         except AttributeError:
             self.fail(
-                "the forecasting should the method get_current_inaccurency'")
+                "the forecasting should have the method get_current_inaccurency'")
+                
+
+        
