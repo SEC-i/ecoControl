d4661dcf9720784523a20c5ebfff9e914615ab45
maschler <fabian.maschler@student.hpi.uni-potsdam.de>
2014-04-17 17:22:21 +0200

    location search working but history-weather-url incomplete (station parameter) #164 [ci skip]

--- simulation/tools/weather_crawler.py
+++ simulation/tools/weather_crawler.py
@@ -3,16 +3,37 @@ from time import gmtime, strftime
 
 day_values = []
 
-def crawl(start, end):
+def find_location(name):
+
+    url = "http://freemeteo.de/wetter/search/?q=%s&language=german&country=germany"
+    D = download.Download()
+    html = D.get(url % name)
+    first_result = None
+    result = {}
+    for entry in xpath.search(html, "//div[@class='today table']//table/tbody/tr"):
+        country = xpath.search(entry, "/td")[2]
+        first_column = xpath.search(entry, "/td")[0]
+        name = xpath.search(first_column,"//a[1]")[0]
+        path = "http://freemeteo.de" + xpath.search(first_column,"//a[1]/@href")[0]
+        path = path.replace("&amp;","&")
+        path = path.replace("Aktuelles-Wetter/Ort","Historie/taglicher-wetterruckblick")
+
+        result[path] = {'country':country, 'name':name}
+
+        if first_result is None:
+            first_result = result.items()[0]
+    return (first_result, result)
+
+def crawl(url, start, end):
       
     t0 = 1325376000 + 60 * 60 * 24 * start  # 1.1.2012
     D = download.Download()
     for i in range(start, end):
         start1 = gmtime(t0)
         
-        url = "http://freemeteo.de/wetter/potsdam/Historie/taglicher-wetterruckblick/?gid=2852458&station=3306&date=%s&language=german&country=germany"
+        #url = "http://freemeteo.de/wetter/potsdam/Historie/taglicher-wetterruckblick/?gid=2852458&station=3306&date=%s&language=german&country=germany"
         
-        html = D.get(url % strftime("%Y-%m-%d",start1))
+        html = D.get(url + "&date=" + strftime("%Y-%m-%d",start1))
         vals = []
         for row in xpath.search(html,"//table[@class='daily-history']/tbody/tr"):
             try:
@@ -37,8 +58,10 @@ def crawl(start, end):
         day_values.extend(vals)
 
 
+cities =  find_location("Potsdam")
+print cities[0]
 
-crawl(0,1)
+crawl(cities[0][0],3,5)
 
 print len(day_values)
 print day_values
