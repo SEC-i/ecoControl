23a085e29d34d13631b730f44d184f2322b13895
Eva-Maria Herbst <eva-maria.herbst@student.hpi.uni-potsdam.de>
2014-04-28 16:41:50 +0200

    test if db has a device and sensor entry for weather forescast

--- server/forecasting/tests/test_weather_forecast.py
+++ server/forecasting/tests/test_weather_forecast.py
@@ -1,13 +1,18 @@
 #import unittest
 from server.forecasting.forecasting.weather import WeatherForecast
 from django.test import TestCase
-#from server.models import Device, Sensor, SensorEntry
+from server.models import Sensor, Device
 
 ''''class ForecastingTest(unittest.TestCase):
 	def test_test(self):
 		cast = WeatherForecast()
 '''
 class ForecastingDBTest(TestCase):
+	def test_if_weather_sources_in_db(self):
+		source = Device.objects.get(name='Internet')
+		sensors = Sensor.objects.filter(device=source)
+		assertGreater(len(sensor), 0)
+		
 	def test_crawled_data_in_data(self):
 		pass
 
