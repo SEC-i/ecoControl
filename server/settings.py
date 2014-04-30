1f8c381da6ee3a7cef6973570ccedd84ef21dd8e
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-04-30 13:17:50 +0200

    Use postgres by default, use sqlite database for testing purposes only

--- server/settings.py
+++ server/settings.py
@@ -58,26 +58,26 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
-        'TEST_NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
-        'OPTIONS': {
-            'timeout': 20,
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
+            'TEST_NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
+            'OPTIONS': {
+                'timeout': 20,
+            }
         }
     }
-}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'bp2013h1_forecasting',
-#             'USER': 'bp2013h1',
-#             'PASSWORD': 'hirsch',
-#             'HOST': 'localhost',
-#             'PORT': '5432',
-#     }
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'bp2013h1',
+                'HOST': 'localhost',
+                'PORT': '5432',
+        }
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
