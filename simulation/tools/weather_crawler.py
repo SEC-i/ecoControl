cf3089f986fb14aa9afedf5cdabb9f9b9923a166
fabian <fabian.maschler@student.hpi.uni-potsdam.de>
2014-05-08 12:27:40 +0200

    Fix crawling url (follow redirect) solves #164

--- simulation/tools/weather_crawler.py
+++ simulation/tools/weather_crawler.py
@@ -1,5 +1,6 @@
 from webscraping import download, xpath
 from time import gmtime, strftime
+from urllib2 import urlopen
 
 day_values = []
 
@@ -16,6 +17,9 @@ def find_location(name):
         name = xpath.search(first_column,"//a[1]")[0]
         path = "http://freemeteo.de" + xpath.search(first_column,"//a[1]/@href")[0]
         path = path.replace("&amp;","&")
+        # Follow Temp Redirect (307) to find right url
+        request = urlopen(path)
+        path = request.geturl()
         path = path.replace("Aktuelles-Wetter/Ort","Historie/taglicher-wetterruckblick")
 
         result[path] = {'country':country, 'name':name}
@@ -25,14 +29,14 @@ def find_location(name):
     return (first_result, result)
 
 def crawl(url, start, end):
-      
-    t0 = 1325376000 + 60 * 60 * 24 * start  # 1.1.2012
+
+    t0 = 1325376000 + 60 * 60 * 24 * (start-1)  # 1.1.2012
     D = download.Download()
     for i in range(start, end):
         start1 = gmtime(t0)
-        
+
         #url = "http://freemeteo.de/wetter/potsdam/Historie/taglicher-wetterruckblick/?gid=2852458&station=3306&date=%s&language=german&country=germany"
-        
+
         html = D.get(url + "&date=" + strftime("%Y-%m-%d",start1))
         vals = []
         for row in xpath.search(html,"//table[@class='daily-history']/tbody/tr"):
@@ -44,7 +48,7 @@ def crawl(url, start, end):
                 print "value error"
             except:
                 print "error in xpath "
-            
+
 
         t0 += 60 * 60 * 24
         if len(vals) < 24 and len(vals) != 0:
@@ -56,12 +60,12 @@ def crawl(url, start, end):
             print "no values found, use yesterday data"
             vals = day_values[-24:]
         day_values.extend(vals)
+    return day_values
 
 
 cities =  find_location("Potsdam")
-print cities[0]
+print "Found %s cities." % len(cities[1])
 
-crawl(cities[0][0],3,5)
+crawl(cities[0][0],1,3)
 
-print len(day_values)
-print day_values
+print "Found %s temperature values: %s" % (len(day_values), day_values)
