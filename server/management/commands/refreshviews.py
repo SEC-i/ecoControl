0c19cdcc587d88a6af983a0330e0f8d128a8970c
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-05-16 22:12:02 +0200

    Update refreshviews command

--- server/management/commands/refreshviews.py
+++ server/management/commands/refreshviews.py
@@ -2,9 +2,10 @@ from django.core.management.base import BaseCommand
 from django.db import connection
 
 class Command(BaseCommand):
-    help = 'Refresh materialized view for aggregated sensorvalues in the database'
+    help = 'Refresh materialized views for aggregated sensorvalues in the database'
 
     def handle(self, *args, **options):
         cursor = connection.cursor()
         cursor.execute('''REFRESH MATERIALIZED VIEW server_sensorvaluehourly;''')
+        cursor.execute('''REFRESH MATERIALIZED VIEW server_sensorvaluemonthly;''')
         self.stdout.write('Successfully refreshed views')
