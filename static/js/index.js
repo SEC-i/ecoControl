c8247b501533152f1ef51420dc2faf0007ca1bfd
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-05-17 19:56:58 +0200

    Enable auto-redirect

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,23 @@
 // READY
 $(function() {
-    $('#login_form').submit(function(event) {
-        login_user();
-        event.preventDefault();
-    });    
+    $.getJSON('/api/status/', function(status_data) {
+        if (status_data['login'] == 'active') {
+            if (status_data['technician']) {
+                if (status_data['system_status'] == 'init') {
+                    redirect_to_tech_settings();
+                } else {
+                    redirect_to_tech();
+                }
+            }else if (status_data['manager']) {
+                redirect_to_mgmt();
+            }
+        } else {
+            $('#login_form').submit(function(event) {
+                login_user();
+                event.preventDefault();
+            }); 
+        }
+    });   
 });
 
 function login_user() {
@@ -29,10 +43,14 @@ function login_user() {
     });
 }
 
-function redirect_to_tech(show) {
+function redirect_to_tech() {
     window.location.href = 'tech/index.html';
 }
 
-function redirect_to_mgmt(show) {
+function redirect_to_tech_settings() {
+    window.location.href = 'tech/settings.html';
+}
+
+function redirect_to_mgmt() {
     window.location.href = 'mgmt/index.html';
 }
