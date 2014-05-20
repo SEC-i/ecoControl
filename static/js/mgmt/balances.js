761e1fd5b0009a3a3bc03eb87e192e2bd628aeeb
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-05-20 19:17:05 +0200

    Add monthly rewards and costs diagrams

--- static/js/mgmt/balances.js
+++ static/js/mgmt/balances.js
@@ -1,3 +1,5 @@
+var diagram_types = ['balances', 'rewards', 'costs'];
+
 // READY
 $(function() {
     initialize_diagram();
@@ -5,33 +7,35 @@ $(function() {
 });
 
 function initialize_diagram() {
-    $('#diagram_container').highcharts({
-        chart: {
-            zoomType: 'xy'
-        },
-        title: {
-            text: ''
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        yAxis: [{
-            labels: {
-                format: '{value} €',
-                style: { color: Highcharts.getOptions().colors[1] }
+    $.each(diagram_types, function(index, type) {
+        $('#' + type + '_container').highcharts({
+            chart: {
+                zoomType: 'xy'
             },
             title: {
-                text: 'Balances in €',
-                style: { color: Highcharts.getOptions().colors[1] }
+                text: 'Monthly ' + type.charAt(0).toUpperCase() + type.slice(1)
             },
-        }],
-        tooltip: {
-            valueDecimals: 2
-        },
-        series: [],
-        credits: {
-            enabled: false
-        }
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            yAxis: [{
+                labels: {
+                    format: '{value} €',
+                    style: { color: Highcharts.getOptions().colors[1] }
+                },
+                title: {
+                    text: 'Balances in €',
+                    style: { color: Highcharts.getOptions().colors[1] }
+                },
+            }],
+            tooltip: {
+                valueDecimals: 2
+            },
+            series: [],
+            credits: {
+                enabled: false
+            }
+        });
     });
 }
 
@@ -44,34 +48,56 @@ function initialize_diagram_filters() {
                 ' + year + '</label>');
         });
         $('.diagram_filter').change(function () {
-            var chart = $('#diagram_container').highcharts();
+
+            charts = [];
+            $.each(diagram_types, function(index, type) {
+                charts.push($('#' +type + '_container').highcharts());
+            });
             var year = parseInt($(this).val());
             if ($(this).is(":checked")) {
                 var found = false;
-                $.each(chart.series, function(index, series) {
-                    if (series['name'].substr(series['name'].length - 4) == year) {
-                        series.show(false);
-                        found = true;
-                    }
+                $.each(charts, function(index, chart) {
+                    $.each(chart.series, function(index2, series) {
+                        if (series['name'].substr(series['name'].length - 4) == year) {
+                            series.show(false);
+                            found = true;
+                        }
+                    });
                 });
                 if (!found) {
                     $.getJSON("/api2/balance/total/" + year + "/", function(data) {
-                        var series_data = {
+                        var balances = {
                             name: 'Total Balances in ' + year,
                             type: 'column',
                             data: []
                         };
+                        var rewards = {
+                            name: 'Total Rewards in ' + year,
+                            type: 'column',
+                            data: []
+                        };
+                        var costs = {
+                            name: 'Total Costs in ' + year,
+                            type: 'column',
+                            data: []
+                        };
                         $.each(data, function(date, monthly_data) {
-                            series_data['data'].push(monthly_data.balance);
+                            balances['data'].push(monthly_data.balance);
+                            rewards['data'].push(monthly_data.rewards);
+                            costs['data'].push(monthly_data.costs);
                         });
-                        chart.addSeries(series_data);
+                        charts[0].addSeries(balances);
+                        charts[1].addSeries(rewards);
+                        charts[2].addSeries(costs);
                     });
                 }
             } else {
-                $.each(chart.series, function(index, series) {
-                    if (series['name'].substr(series['name'].length - 4) == year) {
-                        series.hide(false);
-                    }
+                $.each(charts, function(index, chart) {
+                    $.each(chart.series, function(index, series) {
+                        if (series['name'].substr(series['name'].length - 4) == year) {
+                            series.hide(false);
+                        }
+                    });
                 });
             }
 
@@ -83,13 +109,17 @@ function initialize_diagram_filters() {
                 }
             });
             if (all_unchecked) {
-                $.each(chart.series, function(index, series) {
-                    series.setVisible(true, false);
+                $.each(charts, function(index, chart) {
+                    $.each(chart.series, function(index, series) {
+                        series.setVisible(true, false);
+                    });
                 });
             }
 
-            // finally redraw the chart
-            chart.redraw();
+            // finally redraw all charts
+            $.each(charts, function(index, chart) {
+                chart.redraw();
+            });
         });
 
         // preselect first filter
