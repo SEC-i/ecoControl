375d2ae0a69f552ae4fc57476d9cffb48a8387fa
Max Reimann <max.reimann@student.hpi.uni-potsdam.de>
2014-05-21 19:16:40 +0200

    fixed gap in diagram #242, also fixed wrong values at simulation start #248

--- server/management/__init__.py
+++ server/management/__init__.py
@@ -59,9 +59,11 @@ def install_devices(**kwargs):
         sensors.append(
             Sensor(device=plb, name='Current Gas Consumption', key='current_gas_consumption', unit='kWh', aggregate_sum=True))
         sensors.append(Sensor(device=tc, name='Thermal Consumption',
-                       key='get_consumption_power', unit='kWh', in_diagram=True, aggregate_sum=True))
+                       key='current_power', setter='current_power', unit='kWh', in_diagram=True, aggregate_sum=True))
         sensors.append(Sensor(device=tc, name='Warm Water Consumption',
                        key='get_warmwater_consumption_power', unit='kWh', in_diagram=True, aggregate_sum=True))
+        sensors.append(Sensor(device=tc, name='Room Temperature',
+                       key='temperature_room', setter='temperature_room', unit='°C', aggregate_avg=True))
         sensors.append(Sensor(device=tc, name='Outside Temperature',
                        key='get_outside_temperature', unit='°C', in_diagram=True, aggregate_avg=True))
         sensors.append(Sensor(device=ec, name='Electrical Consumption',
--- server/views.py
+++ server/views.py
@@ -105,8 +105,8 @@ def settings(request):
 
 def forecast(request):
     try:
-        latest_value = SensorValue.objects.latest('timestamp')
-        initial_time = calendar.timegm(latest_value.timestamp.utctimetuple())
+        latest_timestamp = functions.get_past_time()
+        initial_time = calendar.timegm(latest_timestamp.timetuple())
     except SensorValue.DoesNotExist:
         initial_time = time()
     if request.method == 'POST':
