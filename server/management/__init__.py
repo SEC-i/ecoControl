313c902404eaf60c8b6aabb0f27b04e3f118a019
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-05-26 14:09:17 +0200

    Fix imports

--- server/management/__init__.py
+++ server/management/__init__.py
@@ -1,12 +1,7 @@
 import sys
-import datetime
 
-from django.utils.timezone import utc
-from django.db import connection, ProgrammingError
 from django.db.models.signals import post_syncdb
 
-from server.forecasting.systems.data import outside_temperatures_2013, outside_temperatures_2012
-from server.models import Device, Sensor, Configuration, DeviceConfiguration, SensorValueDaily, SensorValueHourly, SensorValueMonthlyAvg, SensorValueMonthlySum, WeatherValue
 from server.management import defaults
 
 
