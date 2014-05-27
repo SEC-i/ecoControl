955cb050442763ad3ada9e2708a3e9c35eb47e03
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-05-27 19:27:08 +0200

    Revert RequireJS changes [ci skip]

---
+++ static/js/ready.js
@@ -0,0 +1,37 @@
+var status_data = null;
+
+$(function() {
+    $.getJSON('/api/status/', function(data) {
+        status_data = data;
+        if (is_logged_in()) {
+            initialize_page(function() {
+                if (status_data['technician'] && status_data['system_status'] == 'init') {
+                    $.address.value('settings');
+                } else {
+                    load_page(get_current_page());
+                }
+            });
+        } else {
+            load_page('login'); 
+        }
+    }).done(function() {
+        $.address.change(function(event) {
+            selected_page = event.value.replace('/', '');
+            if (selected_page == '') {
+                selected_page = 'overview';
+            }
+            load_page(selected_page);
+        });
+    });
+
+    $('.navbar-brand').click(function(e) {
+        if (is_logged_in()) {
+            $('.nav li').removeClass('active');
+            $('.nav li').first().addClass('active');
+            load_page('overview')
+        } else {
+            load_page('login'); 
+        }
+        e.preventDefault();
+    });
+});
