dbd48d427ce1dac1c875aacfcb0e3cf743c48882
MaxReimann <max.reimann@student.hpi.uni-potsdam.de>
2014-07-14 20:19:53 +0200

    :alien: double seasonal holtwinters with autoregression AR(1) fitting

--- server/forecasting/forecasting/holt_winters.py
+++ server/forecasting/forecasting/holt_winters.py
@@ -14,65 +14,84 @@ References:
 from scipy.optimize import fmin_l_bfgs_b
 
 
-def exponential_smoothing_step(input, index, (alpha, beta, gamma), (level, trend, seasonal,forecast), type):
-    # 0 = linear, 1 = addditive, 2 = multiplicative
+def exponential_smoothing_step(input, index, 
+                               (alpha, beta, gamma, delta,autocorrelation),
+                               (forecast, level, trend, seasonal, seasonal2),
+                               hw_type ):
+    # 0 = linear, 1 = addditive, 2 = multiplicative, 3 = double seasonal
     i = index
     Y = input
     
     a = level
     b = trend
     s = seasonal
+    s2 = seasonal2
     y = forecast
-    if type == 0:
+    if hw_type == 0:
         a.append(alpha * Y[i] + (1 - alpha) * (a[i] + b[i]))
         b.append(beta * (a[i + 1] - a[i]) + (1 - beta) * b[i])
         y.append(a[i + 1] + b[i + 1])
     
-    elif type == 1:
+    elif hw_type == 1:
         a.append(alpha * (Y[i] - s[i]) + (1 - alpha) * (a[i] + b[i]))
         b.append(beta * (a[i + 1] - a[i]) + (1 - beta) * b[i])
         s.append(gamma * (Y[i] - a[i] - b[i]) + (1 - gamma) * s[i])
         y.append(a[i + 1] + b[i + 1] + s[i + 1])
     
-    elif type == 2:
+    elif hw_type == 2:
         a.append(alpha * (Y[i] / s[i]) + (1 - alpha) * (a[i] + b[i]))
         b.append(beta * (a[i + 1] - a[i]) + (1 - beta) * b[i])
         s.append(gamma * (Y[i] / (a[i] + b[i])) + (1 - gamma) * s[i])
         y.append((a[i + 1] + b[i + 1]) * s[i + 1])
         
+    elif hw_type == 3:
+        #see Short-term electricity demand forecasting using 
+        #double seasonal exponential smoothing (Tayler 2003)
+        a.append( alpha * (Y[i] - s2[i] - s[i]) + (1 - alpha) * (a[i]))
+        s.append(delta *  (Y[i] - a[i] - s2[i]) + (1 - delta) * s[i])
+        s2.append(gamma * (Y[i] - a[i] - s[i]) + (1 - gamma) * s2[i])
+        autocorr = autocorrelation * (Y[i] - (a[i] + s[i] + s2[i]))
+        y.append(a[i + 1] + s[i + 1] + s2[i + 1] + autocorr)
+
         
 def _holt_winters(params, *args):
     train = args[0][:]
-    type = args[1]
+    hw_type = args[1]
     m = args[2]
     test_data = args[3]
  
-    if type == 0:
+    if hw_type == 0:
 
         alpha, beta = params
         linear(train, len(test_data), alpha, beta)
-    else:
- 
+
+    if hw_type == 1:
         alpha, beta, gamma = params
-        
-        if type == 1:
-            forecast,a,b,c = additive(train, m, len(test_data), alpha=alpha,beta=beta,gamma=gamma)
-        elif type == 2:
-            forecast,a,b,c = multiplicative(train, m, len(test_data), alpha=alpha,beta=beta,gamma=gamma)
-        else:
- 
-            exit('Type must be either linear, additive or multiplicative')
+        forecast, params, onestepfcs = additive(train, m, len(test_data), alpha=alpha,beta=beta,gamma=gamma)
+    elif hw_type == 2:
+        alpha, beta, gamma = params
+        forecast, params, onestepfcs = multiplicative(train, m, len(test_data), alpha=alpha,beta=beta,gamma=gamma)
+
+    elif hw_type == 3:
+            alpha, beta, gamma, delta, autocorrelation = params
+            forecast, params, onestepfcs = double_seasonal(train, m[0],m[1], len(test_data), 
+                                                             alpha=alpha,beta=beta,gamma=gamma,delta=delta, 
+                                                             autocorrelation=autocorrelation)
+    else:
     
-    return forecast
+        exit('type must be either linear, additive or multiplicative')
+    
+    return forecast, onestepfcs
         
 
 def RMSE(params, *args):
-    forecast = _holt_winters(params,*args)
+    forecast, onestepfcs = _holt_winters(params,*args)
     test_data = args[3]
-    rmse = sqrt(sum([(m - n) ** 2 for m, n in zip(test_data, forecast)]) / len(test_data))
-    penalty = mean_below_penalty(input)
+    train = args[0]
+    rmse_outofsample = sqrt(sum([(m - n) ** 2 for m, n in zip(test_data, forecast)]) / len(test_data))
+    rmse_insample = sqrt(sum([(m - n) ** 2 for m, n in zip(train, onestepfcs)]) / len(train))
     
-    return rmse + penalty
+    return rmse_insample +rmse_outofsample
 
 def MASE(params, *args): 
     input = args[0]
@@ -85,17 +104,7 @@ def MASE(params, *args):
     d = np.abs(  np.diff(training_series) ).sum()/(n-1)
     
     errors = np.abs(testing_series - prediction_series )
-    penalty = mean_below_penalty(input)
-    return errors.mean()/d + penalty
-
-def mean_below_penalty(input, value=0):
-    mean = np.array(input).mean()
-    if mean < value:
-        return 0
-    else:
-        return abs(mean) - value 
-    
-
+    return errors.mean()/d
      
  
 def linear(x, forecast, alpha = None, beta = None):
@@ -106,25 +115,21 @@ def linear(x, forecast, alpha = None, beta = None):
  
         initial_values = array([0.3, 0.1])
         boundaries = [(0, 1), (0, 1)]
-        type = 0#'linear'
+        hw_type = 0#'linear'
  
-        parameters = fmin_l_bfgs_b(RMSE, x0 = initial_values, args = (Y, type), bounds = boundaries, approx_grad = True)
+        parameters = fmin_l_bfgs_b(RMSE, x0 = initial_values, args = (Y, hw_type), bounds = boundaries, approx_grad = True)
         alpha, beta = parameters[0]
  
     a = [Y[0]]
     b = [Y[1] - Y[0]]
     y = [a[0] + b[0]]
-    rmse = 0
- 
+
     for i in range(len(Y) + forecast):
  
         if i == len(Y):
             Y.append(a[-1] + b[-1])
             
         exponential_smoothing_step(Y, i, (alpha, beta, None), (a, b, None, y), 0)
- 
-    rmse = sqrt(sum([(m - n) ** 2 for m, n in zip(Y[:-forecast], y[:-forecast - 1])]) / len(Y[:-forecast]))
-
  
     return Y[-forecast:], alpha, beta, y[:-forecast]
  
@@ -136,9 +141,9 @@ def additive(x, m, forecast, alpha = None, beta = None, gamma = None):
  
         initial_values = array([0.3, 0.1, 0.1])
         boundaries = [(0, 1), (0, 1), (0, 1)]
-        type = 1#'additive'
+        hw_type = 1#'additive'
  
-        parameters = fmin_l_bfgs_b(RMSE, x0 = initial_values, args = (Y, type, m), bounds = boundaries, approx_grad = True,factr=10**6)
+        parameters = fmin_l_bfgs_b(RMSE, x0 = initial_values, args = (Y, hw_type, m), bounds = boundaries, approx_grad = True,factr=10**6)
         alpha, beta, gamma = parameters[0]
  
     a = [sum(Y[0:m]) / float(m)]
@@ -151,13 +156,11 @@ def additive(x, m, forecast, alpha = None, beta = None, gamma = None):
         if i == len(Y):
             Y.append(a[-1] + b[-1] + s[-m])
             
-        exponential_smoothing_step(Y, i, (alpha, beta, gamma), (a, b, s, y), 1)
+        exponential_smoothing_step(Y, i,(alpha, beta, gamma, None), (y, a, b, s, None), 1)
  
-    rmse = sqrt(sum([(m - n) ** 2 for m, n in zip(Y[:-forecast], y[:-forecast - 1])]) / len(Y[:-forecast]))
+    return Y[-forecast:], (alpha, beta, gamma), y[:-forecast]
  
-    return Y[-forecast:], alpha, beta, gamma, rmse
- 
-def multiplicative(x, m, forecast, alpha = None, beta = None, gamma = None, initial_values_optimization=[0.002, 0.0, 0.0002], optimization_type="RMSE"):
+def multiplicative(x, m, forecast, alpha=None, beta=None, gamma=None, initial_values_optimization=[0.002, 0.0, 0.0002], optimization_type="RMSE"):
  
     Y = x[:]
     test_series = []
@@ -165,7 +168,7 @@ def multiplicative(x, m, forecast, alpha = None, beta = None, gamma = None, init
  
         initial_values = array(initial_values_optimization)
         boundaries = [(0, 1), (0, 0.05), (0, 1)]
-        type = 2 #'multiplicative'
+        hw_type = 2 #'multiplicative'
         optimization_criterion = RMSE if optimization_type == "RMSE" else MASE
         
         train_series = Y[:-m*2]
@@ -173,7 +176,8 @@ def multiplicative(x, m, forecast, alpha = None, beta = None, gamma = None, init
         
         Y = train_series
 
-        parameters = fmin_l_bfgs_b(optimization_criterion, x0 = initial_values, args = (train_series, type, m, test_series), bounds = boundaries, approx_grad = True,factr=10**3)
+        parameters = fmin_l_bfgs_b(optimization_criterion, x0 = initial_values, args = (train_series, hw_type, m, test_series), 
+                                   bounds = boundaries, approx_grad = True,factr=10**3)
         alpha, beta, gamma = parameters[0]
     
 
@@ -188,8 +192,46 @@ def multiplicative(x, m, forecast, alpha = None, beta = None, gamma = None, init
         if i >= len(Y):
             Y.append((a[-1] + b[-1]) * s[-m])
             
-        exponential_smoothing_step(Y, i, (alpha, beta, gamma), (a, b, s, y), 2)
+        exponential_smoothing_step(Y, i, (alpha, beta, gamma, None), (y, a, b, s, None), 2)
         
-    return Y[-forecast:], alpha, beta, gamma
+    return Y[-forecast:], (alpha, beta, gamma), y[:-forecast]
 
+
+#m = intraday, m2 = intraweek seasonality
+def double_seasonal(x, m, m2, forecast, alpha = None, beta = None, gamma = None,delta=None,
+                    autocorrelation=None, initial_values_optimization=[0.1, 0.0, 0.2, 0.2, 0.9], optimization_type="RMSE"):
+ 
+    Y = x[:]
+    test_series = []
+    if (alpha == None or beta == None or gamma == None):
+ 
+        initial_values = array(initial_values_optimization)
+        boundaries = [(0, 1), (0, 0.0), (0, 1), (0,1), (0,1)]
+        hw_type = 3 #'double seasonal
+        optimization_criterion = RMSE if optimization_type == "RMSE" else MASE
+        
+        train_series = Y[:-m2*1]
+        test_series = Y[-m2*1:]
+        Y = train_series
+
+        parameters = fmin_l_bfgs_b(optimization_criterion, x0 = initial_values, args = (train_series, hw_type, (m,m2), test_series), 
+                                   bounds = boundaries, approx_grad = True,factr=10**3)
+        alpha, beta, gamma, delta, autocorrelation = parameters[0]
+    
+
+    a = [sum(Y[0:m]) / float(m)]
+    b = [(sum(Y[m2:2 * m2]) - sum(Y[0:m2])) / (m2) ** 2]
+    s = [Y[i] / a[0] for i in range(m)]
+    s2 = [Y[i] / a[0] for i in range(0,m2,m)]
+    y = [a[0] + b[0] + s[0] + s2[0]]
+    
+
+    for i in range(len(Y) + forecast+len(test_series)):
+ 
+        if i >= len(Y):
+            Y.append(a[-1] + b[-1] + s[-m] + s2[-m2])
+            
+        exponential_smoothing_step(Y, i, (alpha, beta, gamma, delta, autocorrelation), (y, a, b, s, s2), 3)
+        
+    return Y[-forecast:], (alpha, beta, gamma, delta, autocorrelation), y[:-forecast]
 
