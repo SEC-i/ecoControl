e4ac32b6d0df29ec28da65f3603537f29f697aa6
Max Reimann <max.reimann@student.hpi.uni-potsdam.de>
2014-07-15 11:28:05 +0200

    generic parameters with namedtuple for holt winters

--- server/forecasting/forecasting/holt_winters.py
+++ server/forecasting/forecasting/holt_winters.py
@@ -3,6 +3,7 @@ from sys import exit
 from math import sqrt
 import numpy as np
 from numpy import array
+from collections import namedtuple
 
 """Holt-Winters algorithms to forecasting
 Original Gist: Andre Queiroz, modified and extended by MR
@@ -14,8 +15,7 @@ References:
 from scipy.optimize import fmin_l_bfgs_b
 
 
-def exponential_smoothing_step(input, index, 
-                               (alpha, beta, gamma, delta,autocorrelation),
+def exponential_smoothing_step(input, index, params,
                                (forecast, level, trend, seasonal, seasonal2),
                                hw_type ):
     # 0 = linear, 1 = addditive, 2 = multiplicative, 3 = double seasonal
@@ -28,29 +28,29 @@ def exponential_smoothing_step(input, index,
     s2 = seasonal2
     y = forecast
     if hw_type == 0:
-        a.append(alpha * Y[i] + (1 - alpha) * (a[i] + b[i]))
-        b.append(beta * (a[i + 1] - a[i]) + (1 - beta) * b[i])
+        a.append(params.alpha * Y[i] + (1 - params.alpha) * (a[i] + b[i]))
+        b.append(params.beta * (a[i + 1] - a[i]) + (1 - params.beta) * b[i])
         y.append(a[i + 1] + b[i + 1])
     
     elif hw_type == 1:
-        a.append(alpha * (Y[i] - s[i]) + (1 - alpha) * (a[i] + b[i]))
-        b.append(beta * (a[i + 1] - a[i]) + (1 - beta) * b[i])
-        s.append(gamma * (Y[i] - a[i] - b[i]) + (1 - gamma) * s[i])
+        a.append(params.alpha * (Y[i] - s[i]) + (1 - params.alpha) * (a[i] + b[i]))
+        b.append(params.beta * (a[i + 1] - a[i]) + (1 - params.beta) * b[i])
+        s.append(params.gamma * (Y[i] - a[i] - b[i]) + (1 - params.gamma) * s[i])
         y.append(a[i + 1] + b[i + 1] + s[i + 1])
     
     elif hw_type == 2:
-        a.append(alpha * (Y[i] / s[i]) + (1 - alpha) * (a[i] + b[i]))
-        b.append(beta * (a[i + 1] - a[i]) + (1 - beta) * b[i])
-        s.append(gamma * (Y[i] / (a[i] + b[i])) + (1 - gamma) * s[i])
+        a.append(params.alpha * (Y[i] / s[i]) + (1 - params.alpha) * (a[i] + b[i]))
+        b.append(params.beta * (a[i + 1] - a[i]) + (1 - params.beta) * b[i])
+        s.append(params.gamma * (Y[i] / (a[i] + b[i])) + (1 - params.gamma) * s[i])
         y.append((a[i + 1] + b[i + 1]) * s[i + 1])
         
     elif hw_type == 3:
         #see Short-term electricity demand forecasting using 
         #double seasonal exponential smoothing (Tayler 2003)
-        a.append( alpha * (Y[i] - s2[i] - s[i]) + (1 - alpha) * (a[i]))
-        s.append(delta *  (Y[i] - a[i] - s2[i]) + (1 - delta) * s[i])
-        s2.append(gamma * (Y[i] - a[i] - s[i]) + (1 - gamma) * s2[i])
-        autocorr = autocorrelation * (Y[i] - (a[i] + s[i] + s2[i]))
+        a.append( params.alpha * (Y[i] - s2[i] - s[i]) + (1 - params.alpha) * (a[i]))
+        s.append(params.delta *  (Y[i] - a[i] - s2[i]) + (1 - params.delta) * s[i])
+        s2.append(params.gamma * (Y[i] - a[i] - s[i]) + (1 - params.gamma) * s2[i])
+        autocorr = params.autocorrelation * (Y[i] - (a[i] + s[i] + s2[i]))
         y.append(a[i + 1] + s[i + 1] + s2[i + 1] + autocorr)
 
         
@@ -123,13 +123,15 @@ def linear(x, forecast, alpha = None, beta = None):
     a = [Y[0]]
     b = [Y[1] - Y[0]]
     y = [a[0] + b[0]]
+    
+    named_parameters = namedtuple("Linear", ["alpha", "beta"], False)(alpha,beta)
 
     for i in range(len(Y) + forecast):
  
         if i == len(Y):
             Y.append(a[-1] + b[-1])
             
-        exponential_smoothing_step(Y, i, (alpha, beta, None), (a, b, None, y), 0)
+        exponential_smoothing_step(Y, i, named_parameters, (a, b, None, y), 0)
  
     return Y[-forecast:], alpha, beta, y[:-forecast]
  
@@ -150,13 +152,14 @@ def additive(x, m, forecast, alpha = None, beta = None, gamma = None):
     b = [(sum(Y[m:2 * m]) - sum(Y[0:m])) / m ** 2]
     s = [Y[i] - a[0] for i in range(m)]
     y = [a[0] + b[0] + s[0]]
- 
+    named_parameters = namedtuple("Additive", ["alpha", "beta","gamma"], False)(alpha,beta,gamma)
+    
     for i in range(len(Y) + forecast):
  
         if i == len(Y):
             Y.append(a[-1] + b[-1] + s[-m])
             
-        exponential_smoothing_step(Y, i,(alpha, beta, gamma, None), (y, a, b, s, None), 1)
+        exponential_smoothing_step(Y, i, named_parameters, (y, a, b, s, None), 1)
  
     return Y[-forecast:], (alpha, beta, gamma), y[:-forecast]
  
@@ -186,13 +189,14 @@ def multiplicative(x, m, forecast, alpha=None, beta=None, gamma=None, initial_va
     s = [Y[i] / a[0] for i in range(m)]
     y = [(a[0] + b[0]) * s[0]]
     
+    named_parameters = namedtuple("Multiplicative", ["alpha", "beta","gamma"], False)(alpha,beta,gamma)
 
     for i in range(len(Y) + forecast+len(test_series)):
  
         if i >= len(Y):
             Y.append((a[-1] + b[-1]) * s[-m])
             
-        exponential_smoothing_step(Y, i, (alpha, beta, gamma, None), (y, a, b, s, None), 2)
+        exponential_smoothing_step(Y, i, named_parameters, (y, a, b, s, None), 2)
         
     return Y[-forecast:], (alpha, beta, gamma), y[:-forecast]
 
@@ -224,14 +228,14 @@ def double_seasonal(x, m, m2, forecast, alpha = None, beta = None, gamma = None,
     s = [Y[i] / a[0] for i in range(m)]
     s2 = [Y[i] / a[0] for i in range(0,m2,m)]
     y = [a[0] + b[0] + s[0] + s2[0]]
-    
+    named_parameters = namedtuple("Multiplicative", ["alpha", "beta","gamma","delta","autocorrelation"], False)(alpha,beta,gamma,delta,autocorrelation)
 
     for i in range(len(Y) + forecast+len(test_series)):
  
         if i >= len(Y):
             Y.append(a[-1] + b[-1] + s[-m] + s2[-m2])
             
-        exponential_smoothing_step(Y, i, (alpha, beta, gamma, delta, autocorrelation), (y, a, b, s, s2), 3)
+        exponential_smoothing_step(Y, i, named_parameters, (y, a, b, s, s2), 3)
         
     return Y[-forecast:], (alpha, beta, gamma, delta, autocorrelation), y[:-forecast]
 
