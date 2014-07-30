0a0295354d25000fa6499206ec94c602294ec655
MaxReimann <max.reimann@student.hpi.uni-potsdam.de>
2014-07-30 22:34:40 +0200

    custom no-database test executer

--- server/tests/__init__.py
+++ server/tests/__init__.py
@@ -8,3 +8,15 @@ class CustomTestSuiteRunner(DiscoverRunner):
         Don't destroy test database
         """
         pass
+
+
+
+class NoDbTestRunner(DiscoverRunner):
+    """ A test runner to test without database creation/deletion. 
+        An example, testing a single file:
+        $ python manage.py test server.forecasting.tests.test_extensions --testrunner=server.tests.NoDbTestRunner
+    """
+    def setup_databases(self, **kwargs):
+        pass
+    def teardown_databases(self, old_config, **kwargs):
+        pass
