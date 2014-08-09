bf0b6efb59a56ab0f38e4984d781e165d0079c61
MaxReimann <max.reimann@student.hpi.uni-potsdam.de>
2014-08-09 23:13:46 +0200

    fix hw plotting

--- server/management/commands/holtwinter_parameters.py
+++ server/management/commands/holtwinter_parameters.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand
-from server.forecasting.optimizing.auto_optimization import simulation_run
 from server.forecasting import DemoSimulation
 import time
 from datetime import datetime
